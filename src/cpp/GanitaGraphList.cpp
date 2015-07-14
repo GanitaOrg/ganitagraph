348ea8e11c59a0baa0a585f43cc2ad9d99741ef2
terry <terry@ganita.org>
2015-07-14 08:20:46 -0400

    Version 0.18.0 - added method for easily and efficiently building ordered list of max values

--- src/cpp/GanitaGraphList.cpp
+++ src/cpp/GanitaGraphList.cpp
@@ -18,6 +18,28 @@ GanitaGraphList::GanitaGraphList(int verbose)
   verbosity = verbose;
 }
 
+uint64_t GanitaGraphList::setId(uint64_t ii)
+{
+  id = ii;
+  return(id);
+}
+
+uint64_t GanitaGraphList::setValue(uint64_t val)
+{
+  value = val;
+  return(value);
+}
+
+uint64_t GanitaGraphList::returnId(void)
+{
+  return(id);
+}
+
+uint64_t GanitaGraphList::returnValue(void)
+{
+  return(value);
+}
+
 uint64_t GanitaGraphList::addNew(uint64_t the_id)
 {
   uint64_t ii;
@@ -63,14 +85,110 @@ uint64_t GanitaGraphList::addNew(uint64_t the_id)
     tmpNode->setId(the_id);
     tmpNode->setValue(1);
     node[ii]->returnEdge(0, ed);
-    cout<<"Previous node: "<<node[ii]->returnId()<<endl;
-    cout<<"New node: "<<the_id<<endl;
-    cout<<"New edge value: "<<ed.returnValue()<<endl;
+    //cout<<"Previous node: "<<node[ii]->returnId()<<endl;
+    //cout<<"New node: "<<the_id<<endl;
+    //cout<<"New edge value: "<<ed.returnValue()<<endl;
     tmpNode->addEdge(ii,ed.returnValue());
     node.push_back(std::make_shared<GanitaNode>(*tmpNode));
     // change previous node to point to new node.
     node[ed.returnValue()]->setEdgeId(0,node.size()-1);
     node[ii]->setEdgeValue(0,node.size()-1);
+  }
+
+  delete tmpNode;
+  return(node.size());
+}
+
+// This method will automatically track a fixed number of max values. 
+// It will automatically update the linked list with any new max 
+// values that are in the top listsize values (i.e. the_id). 
+uint64_t GanitaGraphList::buildMaxList(uint64_t the_id, uint64_t nv, uint64_t listsize)
+{
+  uint64_t ii, newvalue;
+  GanitaEdge ed;
+  GanitaNode *tmpNode = new GanitaNode();
+  // Set GanitaGraphList id to max node index
+  // Set GanitaGraphList value to min node index.
+  // This helps bookkeeping of list. 
+  // Each node has a single edge. 
+  // id of edge points to next smallest node. 
+  // value of edge points to next biggest node. 
+
+  if(node.size() == 0){
+    // add first node
+    tmpNode->setId(the_id);
+    tmpNode->setValue(nv);
+    tmpNode->addEdge(0,0);
+    node.push_back(std::make_shared<GanitaNode>(*tmpNode));
+    delete tmpNode;
+    id = 0;
+    value = 0;
+    return(1);
+  }
+
+  // This method runs fast, because most times 
+  // it does not need to update the list. 
+  if((node.size() >= listsize) && (node[value]->returnId() >= the_id)){
+    // No need to add change the list
+    return(node.size());
+  }
+
+  ii = id;
+  while(node[ii]->returnId() > the_id){
+    if(ii == value){
+      // Cycled back to beginning of list.
+      // From check above, we know node.size() < listsize. 
+      // Add new node at end of list.
+      tmpNode->setId(the_id);
+      tmpNode->setValue(nv);
+      tmpNode->addEdge(id,value);
+      node.push_back(std::make_shared<GanitaNode>(*tmpNode));
+      // change previous node to point to new node.
+      node[ii]->setEdgeId(0,node.size()-1);
+      value = node.size() - 1;
+      node[id]->setEdgeValue(0,node.size()-1);
+      delete tmpNode;
+      return(node.size());
+    } 
+    node[ii]->returnEdge(0, ed);
+    ii = ed.returnId();
+  }
+
+  if(node.size() < listsize){
+    // add new node and point to next node.
+    if(ii == id){id = node.size();}
+    tmpNode->setId(the_id);
+    tmpNode->setValue(nv);
+    node[ii]->returnEdge(0, ed);
+    //cout<<"Previous node: "<<node[ii]->returnId()<<endl;
+    //cout<<"New node: "<<the_id<<endl;
+    //cout<<"New edge value: "<<ed.returnValue()<<endl;
+    tmpNode->addEdge(ii,ed.returnValue());
+    node.push_back(std::make_shared<GanitaNode>(*tmpNode));
+    // change previous node to point to new node.
+    node[ed.returnValue()]->setEdgeId(0,node.size()-1);
+    node[ii]->setEdgeValue(0,node.size()-1);
+  }
+  else {
+    // replace node
+    if(ii == id){id = value;}
+    node[value]->returnEdge(0, ed);
+    newvalue = ed.returnValue();
+    node[ii]->returnEdge(0, ed);
+    node[value]->setId(the_id);
+    node[value]->setValue(nv);
+    node[value]->setEdgeId(0, ii);
+    if(ed.returnValue() == value){
+      node[value]->setEdgeValue(0, newvalue);
+    }
+    else{
+      node[value]->setEdgeValue(0, ed.returnValue());
+      node[ed.returnValue()]->setEdgeId(0,value);
+    }
+    node[id]->setEdgeValue(0, newvalue);
+    node[ii]->setEdgeValue(0, value);
+    node[newvalue]->setEdgeId(0,id);
+    value = newvalue;
   }
 
   delete tmpNode;
@@ -86,6 +204,26 @@ uint64_t GanitaGraphList::dumpList(void)
     cout<<"Node: "<<ii<<" | "<<returnNode(ii)->returnId()
 	<<" | "<<returnNode(ii)->returnValue()<<endl;
     returnNode(ii)->dumpEdgeAll();
+  }
+
+  return(node.size());
+}
+
+uint64_t GanitaGraphList::dumpList2(void)
+{
+  uint64_t ii, count;
+  GanitaEdge ed;
+
+  count = 0; ii = id;
+  cout<<"Number of nodes: "<<returnNumNodes()<<endl;
+  cout<<"id/value: "<<returnId()<<"/"<<returnValue()<<endl;
+  while(count < returnNumNodes()){
+    cout<<"Node: "<<ii<<" | "<<returnNode(ii)->returnId()
+	<<" | "<<returnNode(ii)->returnValue()<<endl;
+    returnNode(ii)->dumpEdgeAll();
+    returnNode(ii)->returnEdge(0, ed);
+    ii = ed.returnId();
+    count++;
   }
 
   return(node.size());
--- src/include/ganita/graph/GanitaGraphList.hpp
+++ src/include/ganita/graph/GanitaGraphList.hpp
@@ -10,7 +10,13 @@ private:
 public:
   GanitaGraphList(void);
   GanitaGraphList(int verbosity);
+  uint64_t setId(uint64_t ii);
+  uint64_t setValue(uint64_t val);
+  uint64_t returnId(void);
+  uint64_t returnValue(void);
   uint64_t addNew(uint64_t the_id);
+  uint64_t buildMaxList(uint64_t the_id, uint64_t nv, uint64_t listsize);
   uint64_t dumpList(void);
+  uint64_t dumpList2(void);
 };
 
